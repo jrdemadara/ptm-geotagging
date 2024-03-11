cba015a8cea10a6bfe3e1aa390754488fc7c6328
Johnny Roger <jrdemadara@protonmail.com>
2024-03-11 18:42:39 +0800

    initialize controller changes

--- app/Http/Controllers/InitializeController.php
+++ app/Http/Controllers/InitializeController.php
@@ -10,14 +10,18 @@ class InitializeController extends Controller
     public function index(Request $request)
     {
         $request->validate([
-            'municipality' => 'required|numeric',
+            'municipality' => 'required',
         ]);
 
+        $code = DB::connection('mysql_tupaics')->table('addresscitymun')
+            ->where('citymunDesc', $request->input('municipality'))
+            ->select('addresscitymun.citymuncode')->get();
+
         $data = DB::connection('mysql_tupaics')->table('recipient')
-            ->where('municipality', $request->input('municipality'))
+            ->where('municipality', $code[0]->citymuncode)
             ->select('recipient.precintno', 'recipient.lastname',
                 'recipient.firstname', 'recipient.middlename',
-                'recipient.extension', 'recipient.birthdate', 'recipient.contactno')->get();
+                'recipient.extension', 'recipient.birthdate', 'recipient.contactno', 'recipient.occupation')->get();
 
         return response()->json($data);
     }
