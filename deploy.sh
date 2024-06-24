293238ba2879b2d21715a33254b682fd8c5b342f
Your Name <jrdemadara@protonmail.com>
2024-06-23 22:46:38 -0700

    refactor: deploy.sh project patch

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#cd /var/www/transpo.ptmkapamilya.org
-#git pull origin main
+#cd /var/www/geoapp.ptmkapamilya.org
+git pull origin main
 yarn install
 composer install --no-dev --optimize-autoloader
 sudo chown -R www-data:www-data /var/www/geoapp.ptmkapamilya.org
-sudo chmod -R 775 /var/www/transpo.ptmkapamilya.org/bootstrap/cache
-sudo chmod -R 775 /var/www/transpo.ptmkapamilya.org /var/www/transpo.ptmkapamilya.org/storage
+sudo chmod -R 775 /var/www/geoapp.ptmkapamilya.org/bootstrap/cache
+sudo chmod -R 775 /var/www/geoapp.ptmkapamilya.org /var/www/geoapp.ptmkapamilya.org/storage
 php artisan route:cache
 php artisan config:cache
 php artisan view:cache
-#cp -r /var/www/transpo.ptmkapamilya.org.old/storage/app/profile /var/www/transpo.ptmkapamilya.org/storage/app/
-php artisan storage:link
+cp -r /var/www/ptm-geotagging/storage/app/profile /var/www/geoapp.ptmkapamilya.org/storage/app/
+#php artisan storage:link
