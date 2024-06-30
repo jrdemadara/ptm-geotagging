b4f81a411ba4ca6f4f8636b50ae01fb7179e5057
Your Name <jrdemadara@protonmail.com>
2024-06-30 04:00:28 -0700

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cp -r /var/www/geoapp.ptmkapamilya.org /var/www/geoapp.ptmkapamilya.org.backup
-git reset --hard
-git checkout -- .
-git clean -fd
-git fetch origin
+# cp -r /var/www/geoapp.ptmkapamilya.org /var/www/geoapp.ptmkapamilya.org.backup
+# git reset --hard
+# git checkout -- .
+# git clean -fd
+# git fetch origin
 yarn install
 composer install --no-dev --optimize-autoloader
 sudo chown -R www-data:www-data /var/www/geoapp.ptmkapamilya.org
