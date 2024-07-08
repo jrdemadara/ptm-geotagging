30eb2f2b37a329c05c261fbec0c460c2957ee795
Your Name <jrdemadara@protonmail.com>
2024-07-07 21:13:26 -0700

    fix: route

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProfilesController;
 use App\Http\Controllers\RegisterAdminController;
 use App\Http\Controllers\RegisterController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,10 +22,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
  */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 //* Public Routes
 Route::get('/v1/uri/municipality', [MunicipalityController::class, 'index']);
