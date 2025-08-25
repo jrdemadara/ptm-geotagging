37200a6d5735a75df344a7510b9023313ea8b1c2
jrdemadara <jrdemadara@protonmail.com>
2025-08-25 08:08:42 +0800

    update codebase

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -12,29 +12,27 @@ class RegisterController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|unique:' . User::class,
-            'email' => 'required|string|email|unique:' . User::class,
-            'municipality' => 'required|string',
-            'password' => 'required|string',
-            'device_id' => 'required|string',
+            "name" => "required|string|unique:" . User::class,
+            "email" => "required|string|email|unique:" . User::class,
+            "municipality" => "required|string",
+            "password" => "required|string",
+            "device_id" => "required|string",
         ]);
 
         User::create([
-            'name' => Str::lower($request->input('name')),
-            'email' => Str::lower($request->input('email')),
-            'municipality' => Str::lower($request->input('municipality')),
-            'password' => Hash::make($request->input('password')),
-            'device_id' => $request->input('device_id'),
-            'is_admin' => false,
-            'is_active' => true,
+            "name" => Str::lower($request->input("name")),
+            "email" => Str::lower($request->input("email")),
+            "municipality" => Str::lower($request->input("municipality")),
+            "password" => Hash::make($request->input("password")),
+            "device_id" => $request->input("device_id"),
+            "is_admin" => false,
+            "is_active" => true,
         ]);
 
         $data = [
-            'message' => "success",
+            "message" => "success",
         ];
 
         return response()->json($data);
-
     }
-
 }
